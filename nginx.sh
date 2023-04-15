d0d26c0a662d562ddcad7b884524f9b575b762da
mustafamj9 <mdmustafajaveed007@gmail.com>
2023-04-16 02:42:47 +0530

    adding git package

--- nginx.sh
+++ nginx.sh
@@ -7,11 +7,14 @@ sudo apt-get update
 sudo apt-get install -y nginx
 
 # Install Ansible
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install -y ansible
+# sudo apt-get install -y software-properties-common
+# sudo apt-add-repository -y ppa:ansible/ansible
+# sudo apt-get update
+# sudo apt-get install -y ansible
 
 # Start Nginx
 sudo systemctl start nginx
+
+# Install Git
+sudo apt-get install git -y
 
