1296dfec63a3a44b4932d3e86e773f6c5cb814c2
mustafamj9 <mdmustafajaveed007@gmail>
2023-04-25 04:36:13 +0530

    adding again anisble

--- nginx.sh
+++ nginx.sh
@@ -7,10 +7,9 @@ sudo apt-get update
 sudo apt-get install -y nginx
 
 # Install Ansible
-# sudo apt-get install -y software-properties-common
-# sudo apt-add-repository -y ppa:ansible/ansible
-# sudo apt-get update
-# sudo apt-get install -y ansible
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-get update
+sudo apt-get install ansible -y
 
 # Start Nginx
 sudo systemctl start nginx
