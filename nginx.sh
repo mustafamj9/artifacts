4847475a14807774ed2e9464d237bd6141dfb324
Md Mustafa Javeed <99660206+mustafamj9@users.noreply.github.com>
2023-04-25 04:41:37 +0530

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -8,10 +8,10 @@ sudo apt-get upgrade
 sudo apt-get install nginx
 
 # Install Ansible
-# sudo apt-get install -y software-properties-common
-# sudo apt-add-repository -y ppa:ansible/ansible
-# sudo apt-get update
-# sudo apt-get install -y ansible
+ sudo apt-get install -y software-properties-common
+ sudo apt-add-repository -y ppa:ansible/ansible
+ sudo apt-get update
+ sudo apt-get install -y ansible
 
 # Start Nginx
 sudo systemctl start nginx
